5a74cc705f47b86644e7ee7683fd47a932e1467b
Kim Goulbourne <kimgoulbourne@Kims-MacBook-Pro.local>
2020-03-02 15:09:37 -0500

    Add some mobile styling

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -3,12 +3,16 @@ import React from 'react';
 function FormField(props) {
   return(
     <div class="form__field">
-      <label for={props.value.name}>{props.value.label}</label>
+      <label for={props.value.name}
+             className="form__field-label">
+        {props.value.label}
+      </label>
       <input type={props.value.type}
-             id={props.value.name}
              name={props.value.name}
              ref={props.value.ref}
-             defaultValue={props.value.defaultValue} />
+             defaultValue={props.value.defaultValue}
+             id={props.value.name}
+             className="form__field-input" />
     </div>
   );
 }
